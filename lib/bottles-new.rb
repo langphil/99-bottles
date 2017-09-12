1030669b27b0ee751ed68f61bcda3e373a911cd9
Phil Lang <plangphoto@gmail.com>
2017-09-12 09:34:03 +0100

    Initial pluralisation of bottle

---
+++ lib/bottles-new.rb
@@ -0,0 +1,9 @@
+class Bottles
+
+  def verse(number)
+    "#{number} bottles of beer on the wall, " +
+    "#{number} bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{number - 1} bottle#{'s' unless(number - 1) == 1} of beer on the wall.\n"
+  end
+end
