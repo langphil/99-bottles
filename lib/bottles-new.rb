fed21e8759d28bb19e428b726ed0e5aef2a0129c
Phil Lang <plangphoto@gmail.com>
2017-09-28 14:02:09 +0100

    Refactored Bottles container class

--- lib/bottles-new.rb
+++ lib/bottles-new.rb
@@ -1,4 +1,45 @@
 class BottleNumber
+  attr_reader :number
+
+  def intitialize(number)
+    @number = number
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def container(number)
+    BottleNumber.new(number).container(number)
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
 end
 
 class Bottles
